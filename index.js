5d07a0f59954f92c8c257382c3a6fb9d572e069e
sohagmia360 <gdsohag360@gmail.com>
2022-05-11 19:01:42 +0600

    start blog application api

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/',(req ,res)=>{
+    res.send("hello word")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on ${port} ...`);
+})
