6fcd92e7b34812916b14a8680feba4552eb0e8cf
sohagmia360 <gdsohag360@gmail.com>
2022-05-11 19:33:34 +0600

    setup some middlewere and add some folder  and routes and schema

---
+++ MongoDBconnection/MongoDBconnection.js
@@ -0,0 +1,5 @@
+ const MongoDBconnection = ()=>{
+  
+}
+
+export default MongoDBconnection
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+import bodyParser from "body-parser"
+import cors from 'cors'
+import dotenv from "dotenv"
 import express from "express"
 const app = express()
 const port = process.env.PORT || 5000
+
+/* middleWere */
+dotenv.config()
+app.use(cors())
+app.use(bodyParser.json())
+
+
 
 app.get('/',(req ,res)=>{
     res.send("hello word")
